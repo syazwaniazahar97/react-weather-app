3a66eafc38a44eb4a3e5cdb8cc88e2ec67f5ce61
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-08 23:46:39 +0800

    added basic weather details in Weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
-      <header>Weather App React</header>
+      <Weather />
       <footer>This project is coded by Nur Syazwani and is {" "}
         < a href='https://github.com/syazwaniazahar97/react-weather-app' target='_blank'>open sourced on Github</a>
       </footer>
