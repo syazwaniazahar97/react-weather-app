b999158ee2c427b1a8c2572f698cebb56fa05cd8
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-08 23:05:28 +0800

    added link to github repositories

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App React
+      <header>Weather App React</header>
+      <footer>This project is coded by Nur Syazwani and is {" "}
+        < a href='https://github.com/syazwaniazahar97/react-weather-app' target='_blank'>open sourced on Github</a>
+      </footer>
     </div>
   );
 }
