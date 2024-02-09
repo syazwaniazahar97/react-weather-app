c6559155ace48bff10241aa37073c316f3677cdb
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-09 20:16:13 +0800

    resolved dependencies issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Weather />
       <footer>This project is coded by Nur Syazwani and is {" "}
-        < a href='https://github.com/syazwaniazahar97/react-weather-app' target='_blank'>open sourced on Github</a>
+        < a href='https://github.com/syazwaniazahar97/react-weather-app' target='blank'>open sourced on Github</a>
       </footer>
     </div>
   );
