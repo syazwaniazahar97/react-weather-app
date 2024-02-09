2b2a8770421f7ee5917894b3464f4ca5c6afaa92
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-09 20:20:24 +0800

    added netlify link in footer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ function App() {
   return (
     <div className="App">
       <Weather />
-      <footer>This project is coded by Nur Syazwani and is {" "}
-        < a href='https://github.com/syazwaniazahar97/react-weather-app' target='blank'>open sourced on Github</a>
+      <footer>
+        This project is coded by Nur Syazwani and is open sourced on{" "}
+        <a
+          href="https://github.com/syazwaniazahar97/react-weather-app"
+          target="blank"
+        >
+          Github
+        </a>
+        {" "}and hosted on{" "}
+        <a
+          href="https://exquisite-blancmange-cb87f5.netlify.app/"
+          target="blank"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );
